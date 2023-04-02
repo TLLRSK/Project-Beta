5212cc81959a3f5d9f129927e56392e4bd3d11bb
TLLRSK <112385550+TLLRSK@users.noreply.github.com>
2023-04-02 20:45:31 +0200

    Porject Beta - Robbertas

---
+++ wp-content/themes/robbertas/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+    <main class="notfound color--white">
+        <h1 class="h--l">ERROR 404</h1>
+        <h2 class="h--s">The page you were looking for isn't here</h2>
+    </main>
+
+<?php get_footer(); ?>
